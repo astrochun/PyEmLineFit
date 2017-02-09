87f0ab6dad17336698011ede97fb749e059922a2
Chun Ly <astro.chun@gmail.com>
2017-02-08 21:58:56 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PyEmLineFit
+Python 2.7 codes to fit nebular emission lines in 1-D spectra
---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+PyEmLineFit
+===========
+
+Package of codes created to fit nebular emission lines in 1-D spectra
+
+How to use the documentation
+----------------------------
+
+import PyEmLineFit as elf
+"""
