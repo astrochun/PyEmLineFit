823efed6b090e16660aafe4d0cf32df16a599293
Chun Ly <astro.chun@gmail.com>
2017-02-08 22:59:21 -0700

    Adjust __init__ to import all codes from import command

--- __init__.py
+++ __init__.py
@@ -7,5 +7,9 @@ Package of codes created to fit nebular emission lines in 1-D spectra
 How to use the documentation
 ----------------------------
 
-import PyEmLineFit as elf
+import PyEmLineFit as ELF
 """
+
+import read_data
+
+#__all__ = ['read_data']
